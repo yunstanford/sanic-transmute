56c42a359eb9e1e765b3ea610a4c6a37fbc2c812
Yun Xu <yunx@zillowgroup.com>
2017-06-05 07:59:44 -0700

    fix failed unittests in travis CI

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,41 +1,37 @@
-import pytest
+import json
 
 
-@pytest.mark.asyncio
-async def test_parsing_path_parameters(app):
+def test_parsing_path_parameters(app):
     request, response = app.test_client.get(
             '/api/v1/user/yun',
         )
     assert response.status == 200
-    user = await response.json()
-    assert user == "yun"
+    user = response.text
+    assert json.loads(user) == "yun"
 
 
-@pytest.mark.asyncio
-async def test_parsing_parameters_optional(app):
+def test_parsing_parameters_optional(app):
     request, response = app.test_client.get(
             '/api/v1/env/',
         )
     assert response.status == 200
-    user = await response.json()
-    assert user == False
+    exist = response.text
+    assert json.loads(exist) == False
 
 
-@pytest.mark.asyncio
-async def test_parsing_parameters_optional_with_value(app):
+def test_parsing_parameters_optional_with_value(app):
     request, response = app.test_client.get(
             '/api/v1/env/?exist=True',
         )
     assert response.status == 200
-    user = await response.json()
-    assert user == True
+    exist = response.text
+    assert json.loads(exist) == True
 
 
-@pytest.mark.asyncio
-async def test_parsing_query_parameters(app):
+def test_parsing_query_parameters(app):
     request, response = app.test_client.get(
             '/multiply?left=3&right=4',
         )
     assert response.status == 200
-    result = await response.json()
-    assert result == 12
+    result = response.text
+    assert json.loads(result) == 12
