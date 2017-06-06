c3a1070e336a65e6da52b65893d0c31437c065ec
Yun Xu <yunx@zillowgroup.com>
2017-06-05 17:55:03 -0700

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # sanic-transmute documentation build configuration file, created by
@@ -247,7 +248,7 @@ html_static_path = ['_static']
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'sanictransmutedoc'
+htmlhelp_basename = 'sanic-transmutedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -273,7 +274,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'sanic_transmute.tex', u'sanic-transmute Documentation',
+    (master_doc, 'sanic-transmute.tex', u'sanic-transmute Documentation',
      u'Yun Xu', 'manual'),
 ]
 
@@ -309,7 +310,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'sanic_transmute', u'sanic-transmute Documentation',
+    (master_doc, 'sanic-transmute', u'sanic-transmute Documentation',
      [author], 1)
 ]
 
@@ -324,7 +325,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'sanic_transmute', u'sanic-transmute Documentation',
+    (master_doc, 'sanic-transmute', u'sanic-transmute Documentation',
      author, 'sanic-transmute', 'One line description of project.',
      'Miscellaneous'),
 ]
