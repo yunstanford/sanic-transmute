31525c87b126bf4d209fc5628f251abe9fee63c7
Yun Xu <yunx@zillowgroup.com>
2018-10-06 16:55:19 -0700

    fixing test_parsing.py

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,46 +1,46 @@
 import json
 
 
-def test_parsing_path_parameters(app):
-    request, response = app.test_client.get(
+async def test_parsing_path_parameters(test_cli):
+    response = await test_cli.get(
             '/api/v1/user/yun',
         )
     assert response.status == 200
-    user = response.text
+    user = await response.text()
     assert json.loads(user) == "yun"
 
 
-def test_parsing_path_parameters_list(app):
-    request, response = app.test_client.get(
+async def test_parsing_path_parameters_list(test_cli):
+    response = await test_cli.get(
             '/api/v1/group/?group=velocity&group=mondev',
         )
     assert response.status == 200
-    user = response.text
+    user = await response.text()
     assert json.loads(user) == "mondev,velocity"
 
 
-def test_parsing_parameters_optional(app):
-    request, response = app.test_client.get(
+async def test_parsing_parameters_optional(test_cli):
+    response = await test_cli.get(
             '/api/v1/env/',
         )
     assert response.status == 200
-    exist = response.text
+    exist = await response.text()
     assert json.loads(exist) == False
 
 
-def test_parsing_parameters_optional_with_value(app):
-    request, response = app.test_client.get(
+async def test_parsing_parameters_optional_with_value(test_cli):
+    response = await test_cli.get(
             '/api/v1/env/?exist=True',
         )
     assert response.status == 200
-    exist = response.text
+    exist = await response.text()
     assert json.loads(exist) == True
 
 
-def test_parsing_query_parameters(app):
-    request, response = app.test_client.get(
+async def test_parsing_query_parameters(test_cli):
+    response = await test_cli.get(
             '/multiply?left=3&right=4',
         )
     assert response.status == 200
-    result = response.text
+    result = await response.text()
     assert json.loads(result) == 12
